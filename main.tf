8b099e7eb776e10e0519cf3c6698458625a1ff2a
huyhoangpham <huyhoangpham@users.noreply.github.com>
2023-11-16 22:19:52 +0100

    feat: Added load balancer and jump-server.

--- main.tf
+++ main.tf
@@ -12,8 +12,8 @@ terraform {
 
 # Configure the Azure Provider
 provider "azurerm" {
-  tenant_id       = "af53965b-fa7a-411b-a95f-5f8c41d501e5"
-  subscription_id = "7132032a-f179-42d9-8a37-3738035ff911"
+  tenant_id       = "fb309cc9-571c-4cd9-a657-5c973d68ea74"
+  subscription_id = "9beb4f20-2de1-408f-820a-9c00d375cc0b"
   features {}
 }
 
@@ -45,6 +45,237 @@ resource "azurerm_subnet" "subnet-db" {
   resource_group_name  = element(azurerm_resource_group.rg-web.*.name, count.index)
   virtual_network_name = element(azurerm_virtual_network.vnet.*.name, count.index)
   address_prefixes     = [cidrsubnet(element(var.vnet_address_space, count.index), 4, 15)]
+}
+
+resource "random_string" "fqdn" {
+  length  = 6
+  special = false
+  upper   = false
+  numeric = false
+}
+
+# Create load balancer with public IP
+resource "azurerm_public_ip" "lbpip" {
+  count               = length(var.location)
+  name                = "lb-pip-${var.prefix}-${count.index}"
+  location            = element(var.location, count.index)
+  resource_group_name = element(azurerm_resource_group.rg-web.*.name, count.index)
+  allocation_method   = "Static"
+  domain_name_label   = "${random_string.fqdn.result}-${var.prefix}-${count.index}"
+}
+
+resource "azurerm_lb" "lb" {
+  count               = length(var.location)
+  name                = "lb-${var.prefix}-${count.index}"
+  location            = element(var.location, count.index)
+  resource_group_name = element(azurerm_resource_group.rg-web.*.name, count.index)
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = element(azurerm_public_ip.lbpip.*.id, count.index)
+
+  }
+}
+
+# Incorporate a simple health probe to make sure the web servers in the backend pool are responding and healthy
+resource "azurerm_lb_backend_address_pool" "bpepool" {
+  count           = length(var.location)
+  loadbalancer_id = element(azurerm_lb.lb.*.id, count.index)
+  name            = "bpepool-${var.prefix}-${count.index}"
+}
+
+resource "azurerm_lb_probe" "probe" {
+  count               = length(var.location)
+  loadbalancer_id     = element(azurerm_lb.lb.*.id, count.index)
+  name                = "probe-${var.prefix}-${count.index}"
+  port                = var.application_port
+  interval_in_seconds = 15
+  number_of_probes    = 2
+}
+
+resource "azurerm_lb_rule" "lbnatrule" {
+  count                          = length(var.location)
+  loadbalancer_id                = element(azurerm_lb.lb.*.id, count.index)
+  name                           = "LBPortMapping"
+  protocol                       = "Tcp"
+  frontend_port                  = var.application_port
+  backend_port                   = var.application_port
+  backend_address_pool_ids       = [element(azurerm_lb_backend_address_pool.bpepool.*.id, count.index)]
+  frontend_ip_configuration_name = "PublicIPAddress"
+  probe_id                       = element(azurerm_lb_probe.probe.*.id, count.index)
+}
+
+# Use Auto-Scaling to create Virtual Machines (VM) for public Web
+resource "azurerm_linux_virtual_machine_scale_set" "scale" {
+  count                           = length(var.location)
+  name                            = "vmss-${var.prefix}-${count.index}"
+  location                        = element(azurerm_resource_group.rg-web.*.location, count.index)
+  resource_group_name             = element(azurerm_resource_group.rg-web.*.name, count.index)
+  sku                             = "Standard_D2s_v3"
+  instances                       = 2
+  computer_name_prefix            = "web-${var.prefix}"
+  admin_username                  = var.admin_user
+  admin_password                  = var.admin_password
+  disable_password_authentication = false
+  custom_data                     = base64encode(file("web.conf"))
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+
+  network_interface {
+    name    = "nic-web-${var.prefix}-${count.index}"
+    primary = true
+
+    ip_configuration {
+      name                                   = "IPConfiguration"
+      primary                                = true
+      subnet_id                              = element(azurerm_subnet.subnet-web.*.id, count.index)
+      load_balancer_backend_address_pool_ids = [element(azurerm_lb_backend_address_pool.bpepool.*.id, count.index)]
+      public_ip_address {
+        name                    = "pip-web-${var.prefix}-${count.index}"
+        idle_timeout_in_minutes = 15
+      }
+    }
+  }
+}
+
+# Configure Auto-Scaling thresholds to scale in or out
+resource "azurerm_monitor_autoscale_setting" "vmss" {
+  count               = length(var.location)
+  name                = "vmss-mon-${var.prefix}-${count.index}"
+  location            = element(var.location, count.index)
+  resource_group_name = element(azurerm_resource_group.rg-web.*.name, count.index)
+  target_resource_id  = element(azurerm_linux_virtual_machine_scale_set.scale.*.id, count.index)
+
+  profile {
+    name = "defaultProfile"
+
+    capacity {
+      default = 2
+      minimum = 2
+      maximum = 10
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = element(azurerm_linux_virtual_machine_scale_set.scale.*.id, count.index)
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "GreaterThan"
+        threshold          = 75
+        metric_namespace   = "microsoft.compute/virtualmachinescalesets"
+        dimensions {
+          name     = "AppName"
+          operator = "Equals"
+          values   = ["App1"]
+        }
+      }
+
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = element(azurerm_linux_virtual_machine_scale_set.scale.*.id, count.index)
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "LessThan"
+        threshold          = 25
+      }
+
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
+  }
+
+  notification {
+    email {
+      send_to_subscription_administrator    = true
+      send_to_subscription_co_administrator = true
+      custom_emails                         = [var.custom_email]
+    }
+  }
+}
+
+# Create jump-server as public VMs to access internal VMs
+resource "azurerm_public_ip" "jumpbox" {
+  count               = length(var.location)
+  name                = "jb-pip-${var.prefix}-${count.index}"
+  location            = element(var.location, count.index)
+  resource_group_name = element(azurerm_resource_group.rg-web.*.name, count.index)
+  allocation_method   = "Static"
+  domain_name_label   = "${random_string.fqdn.result}-${var.prefix}-${count.index}-ssh"
+}
+
+resource "azurerm_network_interface" "jumpbox" {
+  count               = length(var.location)
+  name                = "jb-nic-${var.prefix}-${count.index}"
+  location            = element(var.location, count.index)
+  resource_group_name = element(azurerm_resource_group.rg-web.*.name, count.index)
+
+  ip_configuration {
+    name                          = "IPConfiguration"
+    subnet_id                     = element(azurerm_subnet.subnet-web.*.id, count.index)
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = element(azurerm_public_ip.jumpbox.*.id, count.index)
+  }
+}
+
+resource "azurerm_virtual_machine" "jumpbox" {
+  count                 = length(var.location)
+  name                  = "jb-${var.prefix}-${count.index}"
+  location              = element(var.location, count.index)
+  resource_group_name   = element(azurerm_resource_group.rg-web.*.name, count.index)
+  network_interface_ids = [element(azurerm_network_interface.jumpbox.*.id, count.index)]
+  vm_size               = "Standard_D2s_v3"
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "jumpbox-osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "jumpbox-${var.prefix}-${count.index}"
+    admin_username = var.admin_user
+    admin_password = var.admin_password
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
 }
 
 # Create Security group and rules for web VM
@@ -94,45 +325,20 @@ resource "azurerm_network_security_rule" "nsg-ssh-web-rule" {
   network_security_group_name = element(azurerm_network_security_group.nsg-web.*.name, count.index)
 }
 
-# Create Virtual Machine (VM) for public Web
-resource "azurerm_windows_virtual_machine_scale_set" "scale" {
-  count               = length(var.location)
-  name                = "sc${var.prefix}-${count.index}"
-  location            = element(azurerm_resource_group.rg-web.*.location, count.index)
-  resource_group_name = element(azurerm_resource_group.rg-web.*.name, count.index)
-  sku                 = "Standard_D2s_v3"
-  instances           = 2
-  admin_username      = "adminuser"
-  admin_password      = "Admin+123456"
-
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2019-Datacenter"
-    version   = "latest"
-  }
-
-  os_disk {
-    storage_account_type = "Standard_LRS"
-    caching              = "ReadWrite"
-  }
-
-  network_interface {
-    name    = "nic-web-${var.prefix}-${count.index}"
-    primary = true
-    # Don't apply Security group rules on network interface of VM because it is set on subnet level
-    #network_security_group_id = element(azurerm_network_security_group.nsg-web.*.id, count.index)
-
-    ip_configuration {
-      name      = "internal"
-      primary   = true
-      subnet_id = element(azurerm_subnet.subnet-web.*.id, count.index)
-      public_ip_address {
-        name                    = "pip-web-${var.prefix}-${count.index}"
-        idle_timeout_in_minutes = 15
-      }
-    }
-  }
+resource "azurerm_network_security_rule" "nsg-http-web-rule" {
+  count                       = length(var.location)
+  name                        = "ssh"
+  description                 = "Allow HTTP."
+  priority                    = 102
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = element(azurerm_resource_group.rg-web.*.name, count.index)
+  network_security_group_name = element(azurerm_network_security_group.nsg-web.*.name, count.index)
 }
 
 resource "azurerm_network_interface" "nic-db" {
@@ -156,8 +362,8 @@ resource "azurerm_linux_virtual_machine" "vm-db" {
   resource_group_name = element(azurerm_resource_group.rg-web.*.name, count.index)
   size                = "Standard_D2s_v3"
 
-  admin_username                  = "adminuser"
-  admin_password                  = "Admin+123456"
+  admin_username                  = var.admin_user
+  admin_password                  = var.admin_password
   disable_password_authentication = false
 
   network_interface_ids = [
--- variables.tf
+++ variables.tf
@@ -17,3 +17,22 @@ variable "vnet_address_space" {
     "172.20.0.0/16"
   ]
 }
+
+variable "application_port" {
+  description = "Port that you want to expose to the external load balancer"
+  default     = 80
+}
+
+variable "admin_user" {
+  description = "User name to use as the admin account on the VMs that will be part of the VM scale set"
+  default     = "adminuser"
+}
+
+variable "admin_password" {
+  description = "Default password for admin account"
+  default     = "Admin+123456"
+}
+
+variable "custom_email" {
+  description = "Notification email"
+}
